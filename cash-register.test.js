1b6655994e1a235510cad4051d1a3742d47a2006
steve.yoo <steve.yoo@servicenow.com>
2019-06-11 23:57:56 -0500

    adding test to check if customer should not give change

--- cash-register.test.js
+++ cash-register.test.js
@@ -11,3 +11,7 @@ test('makes sure price of item is > 0', () => {
 test('should give change if amount tendered is greater than price', () => {
   expect(amountTendered - item.price).toBeGreaterThan(0);
 });
+
+test('should not give any change', () => {
+  expect(amountTendered - item.price).toBeLessThanOrEqual(0);
+});
