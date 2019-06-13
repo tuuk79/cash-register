98f6d7612bfe6ab0dab621e50080f735decd9707
Sean <sjgallen@gmail.com>
2019-06-12 22:27:18 -0400

    passed tests and created new ones.

--- cash-register.test.js
+++ cash-register.test.js
@@ -1,4 +1,7 @@
 let item = require('./item.js');
+let amountTendered = item.tendered;
+
+let cashRegister = require('./service/cashReister');
 
 test('makes sure item exists', () => {
 	expect(item).toBeDefined();
@@ -13,6 +16,39 @@ test('should give change if amount tendered is greater than price', () => {
 });
 
 test('should not give any change', () => {
+  item.price = 2;
   expect(amountTendered - item.price).toBeLessThanOrEqual(0);
 
 });
+
+describe('the basic featurs of a register', () => {
+  test('Cash Register Exist', () => {
+    expect(cashRegister).toBeDefined();
+  })
+
+  test('Register to be a Cash Register', () => {
+    expect(typeof cashRegister).toBe(CashRegister);
+  })
+
+  test('Register to have a drawer', () => {
+    expect(cashRegister.drawer).toBeDefined();
+  })
+
+  test('Register to start day empty', () => {
+    expect(cashRegister.drawer.total).toBe(0);
+  })
+
+  test('Cash Register can never be less than zero', () => {
+    expect(cashRegister.drawer.total).not.toBeLessThan(0);
+  })
+
+  test('Cash Register can only hold currency values', () => {
+    stuff = ['dog', 'cat', 'house', '1.99', '23##', '👕', '🍺', '🏒', '2r.22', '1', '.05'];
+    currency = ['1', '.02', '.2', '100.22'];
+    var randomItem = stuff[Math.floor(Math.random() * stuff.length)];
+    var randomCurrency = currency[Math.floor(Math.random() * currency.length)];
+
+    expect(cashRegister.drawer.contains(randomItem)).toBeFalsy();
+    expect(cashRegister.drawer.contains(randomCurrency)).toBeTruthy();
+  })
+}) 
--- item.js
+++ item.js
@@ -1,3 +1,4 @@
 let item = {};
 item["price"] = 1;
+item['tendered'] = 2;
 module.exports = item;
